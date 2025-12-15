9f9b6132e138a6122f6d46c2d4204a421c233eef
Baerkanogue <baerkanogue@gmail.com>
2025-12-15 01:11:57 +0100

    update qtui shell script

--- update_qtui.sh
+++ update_qtui.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-rm scripts/gui/qtui.py
+rm scripts/gui/qtmain.py
+rm scripts/gui/qtinfo.py
 
-pyuic6 -x qt/main.ui -o ./scripts/gui/qtui.py
+pyuic6 -x qt/main.ui -o ./scripts/gui/qtmain.py
+pyuic6 -x qt/info.ui -o ./scripts/gui/qtinfo.py
 
-echo "main.ui -> qtui.py"
+echo "main.ui -> qtmain.py"
+echo "info.ui -> qtinfo.py"
