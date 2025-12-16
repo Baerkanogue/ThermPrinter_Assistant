29c481db6c1079c661ce5b825dc4a15d7475858f
Baerkanogue <baerkanogue@gmail.com>
2025-12-16 01:01:19 +0100

    updated export shell script to support auto build

--- export.sh
+++ export.sh
@@ -1,3 +1,11 @@
+source .venv/bin/activate
+
+pyinstaller --onefile --add-data "samples/sample.jpg:samples" --add-data "icons/printer3d.ico:icons" --add-data "icons/logo.ico:icons" --name "ThermPrinter_Assistant" scripts/gui/main.py
+
+pyinstaller --onefile --name "ThermPrinter_Assistant_TUI" scripts/tui/main.py
+
+pyinstaller --onefile --name "cm_inch_converter" scripts/cm_inch_convert.py
+
 mkdir export
 mkdir export/misc
 
