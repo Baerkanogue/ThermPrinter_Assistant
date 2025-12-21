7df50f853686bd77bdf0526768d14f624ae0a952
Baerkanogue <baerkanogue@gmail.com>
2025-12-21 17:56:11 +0100

    export script refined

--- export.sh
+++ export.sh
@@ -1,9 +1,7 @@
 source .venv/bin/activate
 
 pyinstaller --onefile --add-data "samples/sample.jpg:samples" --add-data "icons/printer3d.ico:icons" --add-data "icons/logo.ico:icons" --name "ThermPrinter_Assistant" scripts/gui/main.py
-
 pyinstaller --onefile --name "ThermPrinter_Assistant_TUI" scripts/tui/main.py
-
 pyinstaller --onefile --name "cm_inch_converter" scripts/cm_inch_convert.py
 
 mkdir export
@@ -21,3 +19,4 @@ cd export
 tar -czvf ThermPrinter_Assistant_linux_VERSION.tar.gz .
 cd -
 
+echo export preparations done
